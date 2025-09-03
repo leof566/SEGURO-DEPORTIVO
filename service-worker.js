373567cc0772be4c3a64070fc351a7452ed36f50
leonardo diaz <leonardodiaz3438@gmail.com>
2025-09-03 12:18:15 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+// Basic offline cache
+const CACHE = "asegurados-cache-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./data.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  if (req.url.endsWith("data.json")) {
+    event.respondWith(
+      fetch(req).then(r => { caches.open(CACHE).then(c => c.put(req, r.clone())); return r; })
+      .catch(() => caches.match(req))
+    );
+    return;
+  }
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req)));
+});
